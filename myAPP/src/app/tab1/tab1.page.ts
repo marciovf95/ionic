1deb61fa748ccea072c8d240776b77721f870320
Marcio Vieira Ferreira <marciovf95@gmail.com>
2021-11-24 13:49:29 -0300

    Toast

--- myAPP/src/app/tab1/tab1.page.ts
+++ myAPP/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -8,7 +9,7 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController) {}
+  constructor(public alertController: AlertController,public toastController: ToastController) {}
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
@@ -24,6 +25,7 @@ export class Tab1Page {
         }, {
           text: 'Sim, favoritar',
           handler: () => {
+            this.apresentarToast();
             console.log('Confirm Okay');
           }
         }
@@ -32,5 +34,15 @@ export class Tab1Page {
 
     await alert.present();
   }
+
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'Favoritado',
+      duration: 2000,
+      color:'success'
+    });
+    toast.present();
+  }
+
 
 }
