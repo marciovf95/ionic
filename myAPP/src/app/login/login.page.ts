d3d2f149cfa11eacd8b4248a4885f35adc216ce0
Marcio Vieira Ferreira <marciovf95@gmail.com>
2021-12-25 22:54:21 -0300

    Login

--- myAPP/src/app/login/login.page.ts
+++ myAPP/src/app/login/login.page.ts
@@ -1,4 +1,7 @@
+import { core } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  email:string;
+  senha:string;
+  constructor(public toastController: ToastController,private route:Router) { }
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  login(){
+
+    console.log(this.email)
+    console.log(this.senha)
+    if (this.email==='admin@admin.com'&&this.senha==='admin'){
+      this.route.navigateByUrl('/tabs/tab1');
+      this.presentToast('Bem Vindo','success');
+    }else{
+      this.presentToast('Erro, usuário e/ou senha inválidos! ','danger');
+    }
   }
+    async presentToast(texto:string,cor:string) {
+      const toast = await this.toastController.create({
+        message: texto,
+        color:cor,
+        duration: 2000
+      });
+      toast.present();
+    }
 
+
+  
 }
